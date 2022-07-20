25421e234c0363188bc357de442bbf5cc0b6e498
Albert Casanovas <caay2000@gmail.com>
2022-07-20 15:25:42 +0200

    * prova PR

--- src/main/kotlin/infra/view/Main.kt
+++ src/main/kotlin/infra/view/Main.kt
@@ -6,4 +6,6 @@ fun main(args: Array<String>) {
     // It should create an accountService, do all the operations needed, and print the statement on console
     // Note that the accountService needs a transactionRepository implementation, so it should also be created here!
 
+
+    // aqui posare el meu codi .....
 }
